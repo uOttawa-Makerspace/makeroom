84b9d759c98663e027c51a9c6f9370bf154167f9
Hazimi Asyraf <jimmyasyraf@gmail.com>
2018-03-24 13:53:15 -0400

    add new booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,22 @@
 class BookingsController < ApplicationController
   def index
   end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.end_date = (@booking.start_date + 1.hours).to_datetime if !@booking.end_date.nil?
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render 'new'
+    end
+  end
+
+  def booking_params
+    params.require(:booking).permit(:name, :email, :event_name, :location, :start_date, :end_date)
+  end
 end
