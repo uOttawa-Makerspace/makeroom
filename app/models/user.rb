bfbfc16a938f4a436f4295d2467e0de3d29238d7
Hazimi Asyraf <jimmyasyraf@gmail.com>
2018-03-25 09:29:40 -0400

    add user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:admin, :staff]
+
+  def set_default_role
+    self.role ||= :staff
+  end
+
+  def admin?
+    if role == 'admin'
+      return true
+    else
+      return false
+    end
+  end
 end
---
+++ db/migrate/20180325132515_add_default_role.rb
@@ -0,0 +1,5 @@
+class AddDefaultRole < ActiveRecord::Migration[5.1]
+  def change
+    change_column_default :users, :role, 1
+  end
+end
