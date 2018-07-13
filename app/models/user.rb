8d0b08ece59c944486b657ec2eedc71ecd24fb56
Parastoo-S <psaha091@uottawa.ca>
2018-07-13 13:04:02 -0400

    Added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   enum role: [:admin, :staff, :brunsfield, :sandbox]
+
+  validates :email,
+    presence: { message: "Your email is required." },
+    uniqueness:{message: "Your email is already in use."}
+
+    validates :first_name,
+      presence: {message: "Your first name is required."}
+
+    validates :last_name,
+      presence: {message: "Your last name is required."}
+
+  validates :password,
+    presence: {message: "Please provide a password."}
+
+
 
   def set_default_role
     self.role ||= :staff
