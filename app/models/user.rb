ece80330c7048056f2943f5d609cc869da112570
Parastoo-S <psaha091@uottawa.ca>
2018-08-07 00:04:49 -0400

    Added validations for each field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  belongs_to :organization
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum role: [:admin, :staff, :brunsfield, :sandbox]
