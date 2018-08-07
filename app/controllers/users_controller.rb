aa7c7585149d575c7a9ca00ad6744f1e24ec2d43
Parastoo-S <psaha091@uottawa.ca>
2018-08-06 23:58:16 -0400

    Added parameters to the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
 
 
   def user_params
-    params.require(:user).permit(:name, :email,:password, :password_confirmation, :first_name, :last_name, :organization)
+    params.require(:user).permit(:first_name, :last_name, :email,:password, :password_confirmation, :organization_id )
   end
 end
