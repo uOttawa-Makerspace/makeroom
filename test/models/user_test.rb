32f2d5a64da59cd43c86a492d81e6d6606d097c2
Parastoo-S <psaha091@uottawa.ca>
2018-08-13 18:59:38 -0400

    Added some model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "valid user" do
+    user = User.create(:first_name => "Olivia", :last_name => "Oliver", :username => "olivia", :email => "olivia@gmail.com", :password => "Password1", :organization_id => 1)
+    assert user.valid?, "User is invalid"
+
+    user.organization_id = nil
+    assert user.invalid?, "User is valid"
+
+    user.organization_id = 1
+    user.first_name = nil
+    assert user.invalid?, "User is valid"
+
+    user.first_name = "Olivia"
+    user.last_name = nil
+    assert user.invalid?, "User is valid"
+
+    user.last_name = "Oliver"
+    user.email = nil
+    assert user.invalid?, "User is valid"
+
+    user.email = "olivia@gmail.com"
+    user.password = nil
+    assert user.invalid?, "User is valid"
+
+    user.password = "Password1"
+    user.username = nil
+    assert user.invalid?, "User is valid"
+
+    user.username = "olivia"
+    assert user.valid?, "User is invalid"
+  end
+
 end
