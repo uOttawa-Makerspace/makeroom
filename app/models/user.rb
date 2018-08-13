28b7a344c6e3d66afad1a5261234dc733cf382b6
Parastoo-S <psaha091@uottawa.ca>
2018-08-13 18:57:40 -0400

    Added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,26 +3,39 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   belongs_to :organization
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
   enum role: [:admin, :staff, :brunsfield, :sandbox]
 
   validates :email,
     presence: { message: "Your email is required." },
     uniqueness:{message: "Your email is already in use."}
 
-    validates :first_name,
-      presence: {message: "Your first name is required."}
+  validates :first_name,
+    presence: {message: "Your first name is required."}
 
-    validates :last_name,
-      presence: {message: "Your last name is required."}
+  validates :last_name,
+    presence: {message: "Your last name is required."}
+
+  validates :username,
+    presence: {message: "Username is required."},
+    uniqueness: {message: "Username is already taken."}
 
   validates :password,
-    presence: {message: "Please provide a password."}
+    presence: {message: "Please provide a password."}, :on => :create,
+    confirmation: {message: "Your passwords do not match."}, :on => :create
 
 
 
   def set_default_role
     self.role ||= :staff
+  end
+
+  def set_role(new_role)
+    self.role = new_role
+  end
+
+  def get_full_name
+    self.first_name + " " + last_name
   end
 
   def admin?
