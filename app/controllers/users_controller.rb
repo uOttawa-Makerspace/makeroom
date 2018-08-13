b1b81e21b29a0cc02a1c22a8a470af1cd9bd9b9a
Parastoo-S <psaha091@uottawa.ca>
2018-08-13 18:57:08 -0400

    Controller changes relating to user registration and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,28 @@
 class UsersController < ApplicationController
+  before_action :current_user, except: [:create, :new]
 
   def edit
     @user = current_user
+    if @user.organization_id.present?
+      @user_organization_id = @user.organization_id
+    else
+      @user_organization_id = Organization.first.id
+    end
   end
-
 
   def update_profile
     @user = current_user
     if @user.update(user_params)
+      flash[:notice] = "Profile updated Successfully"
       redirect_to edit_user_path
     else
+      flash[:error] = "Could not save the changes."
       render 'edit'
     end
   end
 
-
+private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email,:password, :password_confirmation, :organization_id )
+    params.require(:user).permit(:first_name, :last_name, :username, :email,:password, :password_confirmation, :organization_id )
   end
 end
