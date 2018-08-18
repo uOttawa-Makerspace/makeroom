0c06c3855ad339c17cdc4315522116f27f66526f
brunomrlima <bruno.mrlima@gmail.com>
2018-08-18 19:24:11 -0400

    flash working

--- db/schema.rb
+++ db/schema.rb
@@ -30,9 +30,11 @@ ActiveRecord::Schema.define(version: 20180809195819) do
   end
 
   create_table "organizations", force: :cascade do |t|
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.index ["user_id"], name: "index_organizations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,5 +60,6 @@ ActiveRecord::Schema.define(version: 20180809195819) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "organizations", "users"
   add_foreign_key "users", "organizations"
 end
