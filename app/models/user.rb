519476de170bd8bb49ef94123f8de5f82ca6e8fa
Parastoo-S <psaha091@uottawa.ca>
2018-08-19 18:34:05 -0400

    Model changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  # has_many: :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   belongs_to :organization
