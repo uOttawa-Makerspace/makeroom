fba4bbf8f4b9db94dea771622895ef088d0e34e8
Parastoo-S <psaha091@uottawa.ca>
2018-08-19 18:33:38 -0400

    Added new rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,19 +3,40 @@ class RoomsController < ApplicationController
     @makerspace_bookings = Booking.where(location: Booking.locations[:makerspace])
   end
 
-  def makerlab
-    @makerlab_bookings = Booking.where(location: Booking.locations[:makerlab])
+  def makerlab119
+    @makerlab119_bookings = Booking.where(location: Booking.locations[:makerlab119])
   end
 
-  def makerlounge
-    @makerlounge_bookings = Booking.where(location: Booking.locations[:makerlounge])
+  def makerlab121
+    @makerlab121_bookings = Booking.where(location: Booking.locations[:makerlab121])
   end
 
-  def brunsfield
-    @brunsfield_bookings = Booking.where(location: Booking.locations[:brunsfield])
+  def mill1
+    @mill1_bookings = Booking.where(location: Booking.locations[:mill1])
+  end
+
+  def lathe1
+    @lathe1_bookings = Booking.where(location: Booking.locations[:lathe1])
+  end
+
+  def lathe2
+    @lathe2_bookings = Booking.where(location: Booking.locations[:lathe2])
+  end
+
+  def welding1
+    @welding1_bookings = Booking.where(location: Booking.locations[:welding1])
+  end
+
+  def welding2
+    @welding2_bookings = Booking.where(location: Booking.locations[:welding2])
   end
 
   def sandbox
     @sandbox_bookings = Booking.where(location: Booking.locations[:sandbox])
   end
+
+  def trailer
+    @trailer_bookings = Booking.where(location: Booking.locations[:trailer])
+  end
+
 end
