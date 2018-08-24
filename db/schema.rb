be422c18f888e560ed05e8e9a363d6d0f3eb016d
Parastoo-S <psaha091@uottawa.ca>
2018-08-24 17:32:07 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180809195819) do
+ActiveRecord::Schema.define(version: 20180824170343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,14 +19,14 @@ ActiveRecord::Schema.define(version: 20180809195819) do
     t.string "name"
     t.string "email"
     t.string "event_name"
-    t.string "staff_id"
-    t.string "staff_name"
     t.integer "location"
     t.boolean "approved"
     t.datetime "start_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "end_date"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "organizations", force: :cascade do |t|
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20180809195819) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "users"
   add_foreign_key "organizations", "users"
   add_foreign_key "users", "organizations"
 end
