ab37908e29d4e588b18e075614efc63f78db8082
Parastoo-S <psaha091@uottawa.ca>
2018-08-29 14:11:55 -0400

    Commented out mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,15 +91,15 @@ Rails.application.configure do
 
 
 
-  config.action_mailer.delivery_method = :smtp
-
-  #GMAIL SETUP
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.sendgrid.net",
-    :port => 587,
-    :user_name => Rails.application.secrets.smtp_user,
-    :password => Rails.application.secrets.smtp_password,
-    :authentication => 'plain',
-    :enable_starttls_auto => true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  #
+  # #GMAIL SETUP
+  # config.action_mailer.smtp_settings = {
+  #   :address => "smtp.sendgrid.net",
+  #   :port => 587,
+  #   :user_name => Rails.application.secrets.smtp_user,
+  #   :password => Rails.application.secrets.smtp_password,
+  #   :authentication => 'plain',
+  #   :enable_starttls_auto => true
+  # }
 end
