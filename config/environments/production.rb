38a35f0791a70f64fe6ed082abb404852b3f1c57
Parastoo-S <psaha091@uottawa.ca>
2018-08-29 18:37:19 -0400

    Trting send_mail method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,15 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'http://makerroom.herokuapp.com',
-    user_name:            '<%= ENV["GMAIL_USER"] %>',
-    password:             '<%= ENV["GMAIL_PASSWORD"] %>',
-    authentication:       'plain',
-    enable_starttls_auto: true }
-
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
