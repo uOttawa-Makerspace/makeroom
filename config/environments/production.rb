c46589cf505b31a9de311d44da7fbb7f60a3095c
Parastoo-S <psaha091@uottawa.ca>
2018-08-29 20:21:03 -0400

    Fixed user and pass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,8 +95,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.sendgrid.net",
     :port => 587,
-    :user_name => '<%= ENV["SMTP_USER"] %>',
-    :password => '<%= ENV["SMTP_PASSWORD"] %>',
+    :user_name => ENV["SMTP_USER"],
+    :password =>  ENV["SMTP_PASSWORD"],
     :authentication => 'plain',
     :domain => 'heroku.com',
     :enable_starttls_auto => true
