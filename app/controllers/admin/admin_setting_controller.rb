7ce01fd2af331514e9fcd9bca364360b81e6f3eb
Parastoo-S <psaha091@uottawa.ca>
2018-08-31 11:15:56 -0400

    Added setting functions

--- app/controllers/admin/admin_setting_controller.rb
+++ app/controllers/admin/admin_setting_controller.rb
@@ -1,6 +1,36 @@
-class Admin::AdminSettingController < ApplicationController
+class Admin::AdminSettingController < AdminAreaController
 
   def index
+    @new_organization = Organization.new
+  end
+
+  def add_organization
+    if !params[:organization][:name].present?
+      flash[:alert] = "Invalid organization name"
+
+    else
+      @new_organization = Organization.new(org_params)
+      @new_organization.save
+      flash[:notice] = "Organization added successfully"
+    end
+
+    redirect_to admin_admin_setting_index_path
+
+  end
+
+  def remove_organization
+    if params[:remove_organization] != ""
+      Organization.where(id: params[:remove_organization]).destroy_all
+      flash[:notice] = "Organization removed successfully"
+    else
+      flash[:alert] = "Please select an organization"
+    end
+    redirect_to admin_admin_setting_index_path
+  end
+
+
+  def org_params
+    params.require(:organization).permit(:name)
   end
 
 
