2bf44bb402c9f839b37f6767b4c2bd22fe9b0fdf
Parastoo-S <psaha091@uottawa.ca>
2018-08-31 17:07:44 -0400

    Added make_staff booking

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,21 +1,32 @@
 class Admin::UsersController < AdminAreaController
-
+  layout 'admin_area'
   def index
-    @users = User.all.paginate(page: params[:page], per_page: 10)
+    if !params[:sort].present? && !params[:direction].present?
+      params[:sort] = "name"
+      params[:direction] = "desc"
+    end
+
+    @users = User.all.order('last_name ASC').paginate(page: params[:page], per_page: 10)
   end
 
-
-  def make_admin
-    binding.pry
-    @user = User.find(params[:id])
-    @user.set_role("admin")
-    @user.skip_password_validation = true
-    if @user.save
-      redirect_to admin_users
+  def search
+    unless params[:query].blank?
+      @query = params[:query]
+      @users = User.all.order('last_name ASC').paginate(page: params[:page], per_page: 10)
+      @found_users = User.where('LOWER(first_name) like LOWER(?) OR
+                           LOWER(last_name) like LOWER(?) OR
+                           LOWER(username) like LOWER(?) OR
+                           LOWER(email) like LOWER(?)',
+                           "%#{@query}%", "%#{@query}%", "%#{@query}%", "%#{@query}%")
+                           .order(:updated_at)
     else
-      redirect_to admin_users
+      redirect_to admin_users_path
+      flash[:alert] = "Invalid parameters!"
     end
   end
+
+
+
 
 
 end
