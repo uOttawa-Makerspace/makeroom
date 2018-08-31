ac280d6d10b6d7b5dc1128b0b0b943ab1447833b
Parastoo-S <psaha091@uottawa.ca>
2018-08-31 11:37:51 -0400

    Used variable for booking

--- app/mailers/room_booking_mailer.rb
+++ app/mailers/room_booking_mailer.rb
@@ -1,25 +1,32 @@
 class RoomBookingMailer < ApplicationMailer
   def new_booking_notification(booking , email)
     @booking = booking
+    @name = booking.name
+    @event_name = booking.event_name
     @email = email
     @location = booking.location_humanize
-    @url = "http://makerroom.herokuapp.com"
-    mail(to: "parastoo.ss@gmail.com", subject: "Makeroom - New booking pending approval")
+    @start_date = booking.start_date
+    @end_date = booking.end_date
+    @repeat = booking.repeat
+    @frequency = booking.frequency
+    @until_date = booking.until_date
+    @url = "http://rooms.makerepo.com/"
+    mail(to: @email, subject: "Makeroom - New booking pending approval")
   end
 
   def booking_confirmation(booking)
     @email = booking.email
     @name = booking.name
-    @event_name = booking.name
+    @event_name = booking.event_name
     @location = booking.location_humanize
-    @url = "http://makerroom.herokuapp.com"
+    @url = "http://rooms.makerepo.com/"
     @start_date = booking.start_date
     @end_date = booking.end_date
     @repeat = booking.repeat
     @frequency = booking.frequency
     @until_date = booking.until_date
 
-    mail(to: "parastoo.ss@gmail.com", subject: "Makeroom - Your booking has been placed")
+    mail(to: @email, subject: "Makeroom - Your booking has been placed")
   end
 
 
@@ -27,14 +34,14 @@ class RoomBookingMailer < ApplicationMailer
     @booking = booking
     @email = booking.email
     @name = booking.name
-    @event_name = booking.name
+    @event_name = booking.event_name
     @location = booking.location_humanize
     @start_date = booking.start_date
     @end_date = booking.end_date
     @repeat = booking.repeat
     @frequency = booking.frequency
     @until_date = booking.until_date
-    mail(to: "parastoo.ss@gmail.com", subject: "Makeroom - Your booking request has been accepted.")
+    mail(to: @email, subject: "Makeroom - Your booking request has been accepted.")
 
   end
 end
